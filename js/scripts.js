e4dd6e0a91827ab313f238b792e60813f9183187
wmkania <wmkania@gmail.com>
2021-05-18 22:56:43 +0100

    Added spaces in pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList=[
-  {name: "bulbasaur", height:7, type: ['grass', 'poison']}
-  {name: "squirtle", height:4, type: 'water'}
-  {name: "jigglypuff", height:2, type: ['fairy', 'normal']}
+  {name: "bulbasaur", height: 7, type: ['grass', 'poison']}
+  {name: "squirtle", height: 4, type: 'water'}
+  {name: "jigglypuff", height: 2, type: ['fairy', 'normal']}
 ];
