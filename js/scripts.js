a69ea2f2988b98fc811dff990f77bf63f42642a4
wmkania <wmkania@gmail.com>
2021-05-21 19:51:24 +0100

    Updated loop and conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 let pokemonList=[
-  {name: "bulbasaur", height: 7, type: ['grass', 'poison']},
-  {name: "squirtle", height: 4, type: 'water'},
-  {name: "jigglypuff", height: 2, type: ['fairy', 'normal']}
+{name: "bulbasaur", height: 7, type: ['grass', 'poison']},
+{name: "squirtle", height: 4, type: 'water'},
+{name: "jigglypuff", height: 2, type: ['fairy', 'normal']}
 ];
 
-
 for (let i = 0; i < pokemonList.length; i++) {
-    if(i == 0){
-    document.write(pokemonList[i].name)
-   console.log(pokemonList[i].name + pokemonList[i].height);
- }
+  if(i === 0){
+      console.log(pokemonList[0].name + pokemonList[0].height);
+  }
+  if(i === 0){
+      document.write(pokemonList[0].name);
+  }
+}
 
- let height = 3;
- let result;
- if (height < 3) {
-   result = 'This is the smallest pokemon';
-   console.log('This is the smallest pokemon!');
- }
+let height = 7;
+let result;
+if (height >= 7) {
+ result = 'This is the biggest pokemon';
+ console.log('This is the biggest pokemon');
+}
